3aaf315a0d5e77668cae1bc4701184482b10d2b7
deprecatum <33914332+deprecatum@users.noreply.github.com>
2019-01-16 15:29:54 +0000

    Update discrete_cosine.vhd

--- discrete_cosine.vhd
+++ discrete_cosine.vhd
@@ -5,8 +5,10 @@ use IEEE.numeric_std.all;
 entity xc9536 is
 	port (
 			clk_in :           in  std_logic;
+			--data events
 			load_data :         in  std_logic;
 			load_done : out std_logic;
+			--dct events
 			dct_start : in  std_logic;
 			dct_end : out  std_logic;
 			--parallel data pins
@@ -24,17 +26,26 @@ architecture hybrid of xc9536 is
 	-- need to store read x, y position, so its able to read next block of pixels
 -- read counter to know when to reset FIFO and get a new frame loaded to FIFO
 begin
-	fetch_data : process(load_data, )
+	fetch_data : process(load_data, ) is
 	begin
 		--cycle thru fifo, fetch a 8x8 block, then enable dct_algorithm
-		load_done => LOW;
-		start => load_data;
-		load_done => HIGH;
-	end fetch_data;
+		if load_data = HIGH then
+			for I in 0 to 7 loop
+				if (A = I) then
+					--load fifo data
+				end if;
+			end loop;
+			load_done <= HIGH;
+		end if;
+	end process fetch_data;
 	
-	dct_algorithm : process(load_data, )
+	dct_algorithm : process(dct_start, load_done, input_data) is
 	begin
 		--apply dct algorithm and output data
-	end fetch_data;
+		if (dct_start = high) and (load_done = high) then 
+			-- do dct
+			-- send data
+		end if;
+	end process fetch_data;
 
 end architecture hybrid;
