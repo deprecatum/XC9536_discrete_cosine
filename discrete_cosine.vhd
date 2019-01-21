eedf2510c66cd163b9cbdecfcb9042020ada8757
deprecatum <33914332+deprecatum@users.noreply.github.com>
2019-01-21 14:36:56 +0000

    Update discrete_cosine.vhd

--- discrete_cosine.vhd
+++ discrete_cosine.vhd
@@ -1,32 +1,39 @@
 use IEEE.numeric_std.all;
 
 entity xc9536 is
-	port (
-			clk_in :           in  std_logic;
-			--data events
-			load_data :         in  std_logic;
-			load_done : out std_logic;
-			--dct events
-			dct_start : in  std_logic;
-			dct_end : out  std_logic;
-			data_out :          out std_logic_vector(0 to 11);
-			
-			--al422b connection pins
-			d0,d1,d2,d3,d4,d5,d6,d7 : in  std_logic_vector(0 to 7); --change to (7 downto 0) if bits need to be inverted 
-			not_output_enable: out std_logic;
-			write_clk: out std_logic;
-			not_write_reset: out std_logic;
-			not_write_enable: out std_logic;
-			read_clk: out std_logic;
-			not_read_reset: out std_logic;
-			not_read_enable: out std_logic;
-		 );
+port (
+	clk                    : in  std_logic;
+	--data events
+	load_data              : in  std_logic;
+	load_done              : out std_logic;
+	--dct events
+	dct_start              : in  std_logic;
+	dct_end                : out  std_logic;
+	data_out               : out std_logic_vector(11 downto 0); --output data with MSB first
+	--al422b connection pins
+	d0,d1,d2,d3,d4,d5,d6,d7: in  std_logic_vector(7 downto 0); --assuming al422b outputs bytes with MSB first 
+	not_output_enable      : out std_logic;
+	write_clk              : out std_logic;
+	not_write_reset        : out std_logic;
+	not_write_enable       : out std_logic;
+	read_clk               : out std_logic;
+	not_read_reset         : out std_logic;
+	not_read_enable        : out std_logic;
+ );
 end entity xc9536;    
 
 
 architecture hybrid of xc9536 is
-	input_data : bit_vector (0 to 7, 0 to 7); --the block of 8 x 8 pixels
-	output_data : bit_vector (0 to 7, 0 to 7); --the block of 8 x 8 pixels
+	clock_input : clk;
+	--define a row to store the 8bit pixels coming from al422
+	type pixel_row is array (0 to 7) of std_logic_vector (7 downto 0);
+	--define the 2d array of pixels
+	type pixel_array is array (0 to 7) of pixel_row (0 to 7);
+	input_data : pixel_array; --the block of 8 x 8 pixels
+	
+	type output_data_array is array(0 to 7) of std_logic_vector(0 to 7);
+	output_data : output_data_array; --output array
+	pointer_position : integer
 	-- need to store read x, y position, so its able to read next block of pixels
 	-- read counter to know when to reset FIFO and get a new frame loaded to FIFO
 begin
@@ -43,14 +50,14 @@ begin
 				
 				not_read_enable => low;
 				--load fifo data
-				input_data(I, 0) <= d0;
-				input_data(I, 1) <= d1;
-				input_data(I, 2) <= d2;
-				input_data(I, 3) <= d3;
-				input_data(I, 4) <= d4;
-				input_data(I, 5) <= d5;
-				input_data(I, 6) <= d6;
-				input_data(I, 7) <= d7;
+				input_data(I)(0) <= d0;
+				input_data(I)(1) <= d1;
+				input_data(I)(2) <= d2;
+				input_data(I)(3) <= d3;
+				input_data(I)(4) <= d4;
+				input_data(I)(5) <= d5;
+				input_data(I)(6) <= d6;
+				input_data(I)(7) <= d7;
 				if (I != 7) then
 					for X in 0 to 639 loop
 						--pulse read clock
@@ -79,5 +86,4 @@ begin
 		end if;
 	end process dct_algorithm;
 
-end architecture hybrid;
 end architecture hybrid;
